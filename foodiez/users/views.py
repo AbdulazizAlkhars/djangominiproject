fb811637888cfdc2c32cc9174486308224bb0c1a
AbdulazizAlkhars <alkharswork@gmail.com>
2022-08-14 21:22:49 +0300

    Login & Logout

--- foodiez/users/views.py
+++ foodiez/users/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
 from . import forms
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 
@@ -10,8 +11,14 @@ def register(request):
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
-            messages.success(request, f"Account created for {username}!")
-            return redirect('recipes-home')
+            messages.success(
+                request, f"Account created for {username}! Please login")
+            return redirect('user-login')
     else:
         form = forms.UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
+
+
+@login_required()
+def profile(request):
+    return render(request, 'users/profile.html')
